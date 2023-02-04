7890ec954af02e33a41a4bee86ba4e6f6849dca1
giannurwana19 <giannurwana19@gmail.com>
2023-02-04 12:23:42 +0700

    membuat hamburger #2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,10 @@
 const menuBar = document.querySelector('.hamburger');
+const menuNav = document.querySelector('.navbar');
 
 menuBar.addEventListener('click', () => {
   menuBar.classList.toggle('is-active');
+});
+
+window.addEventListener('scroll', () => {
+  menuNav.classList.toggle('nav-scroll-active', window.scrollY > 0);
 });
